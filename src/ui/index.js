dd1da9929c76e29eba0896b4e7ebe9d49d9061e9
Evan Winter <evan.winter@brookfieldpropertiesretail.com>
2021-06-16 16:42:10 -0500

    Bare minimum code for React + SCSS + Parcel "hello world" web app

---
+++ src/ui/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import App from "ui/components/App"
+// import "ui/styles/index.scss"
+
+ReactDOM.render(<App />, document.querySelector("#root"))
