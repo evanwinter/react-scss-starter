b2dfd1efcc98282be2748ce274d0356062dd549c
Evan Winter <evan.winter@brookfieldpropertiesretail.com>
2021-06-22 19:24:32 -0500

    Update some things

--- src/ui/components/App.jsx
+++ src/ui/components/App.jsx
@@ -2,10 +2,18 @@ import React from "react"
 
 const App = () => {
   return (
-    <div>
-      Hello world
+    <div className="App">
+      <h1>Et laborum non nisi sunt aute.</h1>
+      <p>
+        In elit aute dolore nostrud veniam nostrud ad ad consequat nisi ipsum cupidatat.
+        Quis magna in culpa adipisicing do adipisicing. Et dolore nulla incididunt amet
+        incididunt adipisicing culpa est cupidatat veniam tempor cupidatat est sunt. Elit
+        ut occaecat duis adipisicing eiusmod magna veniam et mollit. Tempor consequat ut
+        deserunt minim do labore. Nisi quis est laboris incididunt anim pariatur nostrud
+        cupidatat elit commodo incididunt.
+      </p>
     </div>
   )
 }
 
-export default App
+export { App }
--- src/ui/index.js
+++ src/ui/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-import App from "ui/components/App"
-// import "ui/styles/index.scss"
+import { App } from "ui/components/App"
+import "ui/styles/index.scss"
 
 ReactDOM.render(<App />, document.querySelector("#root"))
